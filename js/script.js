3f7db99fb00e0335b17fe89c5446293dbda78c27
Ethan McFarlin <ethanmcfarlin@gmail.com>
2017-12-30 23:07:18 -0700

    Scroll to top widget

--- js/script.js
+++ js/script.js
@@ -116,3 +116,17 @@ $(window).load(function(){
     }
   });
 });
+
+// Scroll to Top
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 500) {        
+        $('#return-to-top').fadeIn(200);    
+    } else {
+        $('#return-to-top').fadeOut(200);   
+    }
+});
+$('#return-to-top').click(function() {      
+    $('body,html').animate({
+        scrollTop : 0                       
+    }, 500);
+});
