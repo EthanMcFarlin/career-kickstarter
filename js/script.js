a049688fa52dd25588174d1029653bf1a5c0b86a
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-01-10 18:39:22 -0700

    Skeleton of Resume Tips

--- js/script.js
+++ js/script.js
@@ -98,10 +98,10 @@ $(window).load(function(){
     var scrollNav = document.querySelector('.scroll-nav');
     var totalScroll = (wintop/(docheight-winheight))*100;
     $(".KW_progressBar").css("width",(totalScroll-12)+"%");
-    if (totalScroll >= 106) {
+    if (totalScroll >= 126) {
         scrollNav.classList += ' scrollNav-hidden';
     }
-    else if (totalScroll >= 105) {
+    else if (totalScroll >= 125) {
       progressBar.classList += ' progressBar-hidden';
       scrollNav.classList.remove('scrollNav-hidden'); 
     }
