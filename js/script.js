89b2760b3fa31db89c37eb3f28dbcd2da43e349f
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-01-14 21:31:41 -0700

    Builder demo

--- js/script.js
+++ js/script.js
@@ -179,3 +179,10 @@ $(document).ready(function() {
 	});
 	
 });
+
+
+
+$('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
